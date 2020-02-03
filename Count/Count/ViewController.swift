a51cb6c9310b5a6ce28f99732fffd600e005d872
konatsup <konatsukomatsu@gmail.com>
2020-02-03 21:11:21 +0900

    数字によって色を変えるようにする

--- Count/Count/ViewController.swift
+++ Count/Count/ViewController.swift
@@ -19,26 +19,37 @@ class ViewController: UIViewController {
     
     @IBAction func plus(){
         number += 1
-        label.text = String(number)
+        setText(num: number)
     }
     
     @IBAction func minus(){
         number -= 1
-        label.text = String(number)
+        setText(num: number)
     }
     
     @IBAction func times2(){
         number *= 2
-        label.text = String(number)
+        setText(num: number)
     }
     
     @IBAction func divide2(){
         number /= 2
-        label.text = String(number)
+        setText(num: number)
     }
     
     @IBAction func clear(){
         number = 0
+        setText(num: number)
+    }
+    
+    func setText(num: Int){
+        if num >= 10 {
+            label.textColor = UIColor.red
+        } else if num <= -10{
+            label.textColor = UIColor.blue
+        } else {
+            label.textColor = UIColor.init(red: 163/255.0, green: 154/255.0, blue: 196/255.0, alpha: 1.0)
+        }
         label.text = String(number)
     }
     
