2b1ff2d48bff0d4cec2f9e1341ac704e4d4558be
konatsup <konatsukomatsu@gmail.com>
2020-02-05 01:45:26 +0900

    Add convertToKanji

--- Count/Count/ViewController.swift
+++ Count/Count/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     var number: Int = 0
-    @IBOutlet var label: UILabel!
+    @IBOutlet var kanjiLabel: UILabel!
+    @IBOutlet var numberLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,14 +44,62 @@ class ViewController: UIViewController {
     }
     
     func setText(num: Int){
-        if num >= 10 {
-            label.textColor = UIColor.red
-        } else if num <= -10{
-            label.textColor = UIColor.blue
+        var color: UIColor!
+        if num >= 0 {
+            color = UIColor.init(red: 163/255.0, green: 154/255.0, blue: 196/255.0, alpha: 1.0)
         } else {
-            label.textColor = UIColor.init(red: 163/255.0, green: 154/255.0, blue: 196/255.0, alpha: 1.0)
+//                        color = UIColor.init(red: 132/255.0, green: 162/255.0, blue: 200/255.0, alpha: 1.0)
+            color = UIColor.init(red: 166/255.0, green: 166/255.0, blue: 166/255.0, alpha: 1.0)
         }
-        label.text = String(number)
+        
+        numberLabel.textColor = color
+        kanjiLabel.textColor = color
+        
+        numberLabel.text = String(num)
+        kanjiLabel.text = convertToKanji(num: num)
+    }
+    
+    func convertToKanji(num: Int) -> String {
+        var bigNum = abs(num)
+        let kanjiNumbers: [String] = ["","壱", "弐", "参", "肆", "伍", "陸", "漆", "捌", "玖"]
+        let smallDigits: [String] = ["", "拾", "佰", "仟"]
+        let bigDigits: [String] = ["", "萬", "億", "兆", "京", "垓"]
+        var kanji = ""
+        var smallCount = 0
+        var bigCount = 0
+        while(true) {
+            var smallNum = bigNum % 10000
+            kanji = bigDigits[bigCount] + kanji
+            smallCount = 0
+            while(true){
+                let rest = smallNum % 10
+                if(rest != 0){
+                    if(rest == 1 && smallCount > 0){
+                        kanji = smallDigits[smallCount] + kanji
+                    } else {
+                        kanji = kanjiNumbers[rest] + smallDigits[smallCount] + kanji
+                    }
+                }
+                if((smallNum - 9) <= 0) { break };
+                smallNum = smallNum / 10
+                smallCount += 1
+            }
+            if((bigNum - 9999) <= 0) { break };
+            bigNum = bigNum / 10000
+            bigCount += 1
+        }
+        
+        if kanji.isEmpty {
+            kanji = "零"
+        }
+        
+        print(num)
+        print(kanji)
+        
+        if(num < 0){
+            kanji = "負" + kanji
+        }
+        return kanji
     }
     
 }
