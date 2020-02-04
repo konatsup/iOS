817fdeb895242d04923ddc9a6af770742333c445
konatsup <konatsukomatsu@gmail.com>
2020-02-05 01:45:08 +0900

    Create #6 MentorBook

---
+++ MentorBook/MentorBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MentorBook
+//
+//  Created by konatsu_p on 2020/02/04.
+//  Copyright © 2020 konatsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
